c4b96933226d44600065f9b817d93f7041e57bc3
remiposo <57336808+remiposo@users.noreply.github.com>
2022-11-17 15:51:08 +0000

    refactor: Refine create

--- handler/create.go
+++ handler/create.go
@@ -23,14 +23,18 @@ func Create(cCtx *cli.Context) error {
 	if _, err := os.Stat(rootDir); err == nil {
 		return createErr(fmt.Sprintf("'%s' already exists", rootDir))
 	}
+
 	if err := os.MkdirAll(rootDir, 0755); err != nil {
 		return createErr(fmt.Sprintf("failed to create '%s'", rootDir))
 	}
-	confPath := filepath.Join(rootDir, "goj.yml")
+	confPath := filepath.Join(rootDir, confFile)
 	if err := os.WriteFile(confPath, []byte{}, 0644); err != nil {
 		return createErr(fmt.Sprintf("failed to create '%s'", confPath))
 	}
-	fmt.Fprintf(cCtx.App.Writer, "created new project in '%v'\n", rootDir)
+
+	fmt.Fprintf(cCtx.App.Writer, "created new project to '%s'\n", rootDir)
+	fmt.Fprintf(cCtx.App.Writer, "next, run download and get samples\n")
+	fmt.Fprintf(cCtx.App.Writer, "(cd '%s' && goj d atcoder abc100)\n", rootDir)
 
 	return nil
 }
--- handler/handler.go
+++ handler/handler.go
@@ -8,17 +8,14 @@ import (
 
 const confFile = "goj.yml"
 
-func findRoot(dir string) (string, error) {
-	dir, err := filepath.Abs(dir)
-	if err != nil {
+func findRoot(curDir string) (string, error) {
+	curDir = filepath.Clean(curDir)
+	if _, err := os.Stat(curDir); err != nil {
 		return "", fmt.Errorf("%s not found", confFile)
 	}
-	if _, err := os.Stat(filepath.Join(dir, confFile)); err == nil {
-		return dir, nil
+	if _, err := os.Stat(filepath.Join(curDir, confFile)); err == nil {
+		return curDir, nil
 	}
 
-	if dir == "/" {
-		return "", fmt.Errorf("%s not found", confFile)
-	}
-	return findRoot(filepath.Dir(dir))
+	return findRoot(filepath.Join(curDir, ".."))
 }
